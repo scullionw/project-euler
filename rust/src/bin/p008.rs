34c399705f6327a92df8e940ddb956f4b0976b58
scullionw <scuw1801@usherbrooke.ca>
2018-10-17 14:47:39 -0400

    p011 generalized

--- rust/src/bin/p008.rs
+++ rust/src/bin/p008.rs
@@ -2,24 +2,25 @@
 
 extern crate euler;
 
-fn solve(seq: &str) -> u64 {
+fn solve(seq: &str, n: usize) -> u64 {
     seq .lines()
         .fold(String::new(), |a, b| a + b)
-        .chars()
+        .chars() // use split("") instead and bench
         .map(|x| x.to_digit(10).unwrap() as u64)
         .collect::<Vec<u64>>()
         .as_slice()
-        .windows(13)
+        .windows(n)
         .map(|x| x.iter().product())
         .max()
         .unwrap()
 }
 
-const PROBLEM_INPUT: &str = include_str!("data/p008_data.txt");
+const PROBLEM_INPUT: usize = 13;
+const PROBLEM_DATA: &str = include_str!("data/p008_data.txt");
 
 fn main() {
-    euler::go(solve, PROBLEM_INPUT);
-    euler::bench(solve, PROBLEM_INPUT, 10);
+    euler::go(|n| solve(PROBLEM_DATA, n), PROBLEM_INPUT);
+    //euler::bench(solve, PROBLEM_INPUT, 10);
 }
 
 #[cfg(test)]
@@ -27,7 +28,8 @@ mod tests {
     use super::*;
     #[test]
     fn correct_answer() {
-        assert_eq!(solve(PROBLEM_INPUT), 23514624000);
+        assert_eq!(solve(PROBLEM_DATA, 4), 5832);
+        assert_eq!(solve(PROBLEM_DATA, PROBLEM_INPUT), 23514624000);
     }
 }
 
@@ -39,6 +41,6 @@ mod bench {
     
     #[bench]
     fn bench_solve(b: &mut Bencher) {
-        b.iter(|| solve(PROBLEM_INPUT));
+        b.iter(|| solve(PROBLEM_DATA, PROBLEM_INPUT));
     }
 }
